2a316f2a5a19a3ca01e364f7dae235f46ea6aa2c
MunYongJang <dltk456@naver.com>
2022-08-12 11:37:56 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:chatting_app/screens/chat_screen.dart';
+import 'package:chatting_app/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Catting app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            return const ChatScreen();
+          }
+          return const LoginSignupScreen();
+        },
+      )
+    );
+  }
+}
